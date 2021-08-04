3a52b1cf06bdf4b53ed7fd4eb714b23f5fd2a8ff
Aleksei A. Leonov <aleksei.leonov@bookedby.com>
2021-08-05 02:44:36 +0300

    errors to stderr

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ -f ${PUBLICIP_FILE} ] && [ -f ${PORT_FORWARDING_STATUS_FILE} ]; then
     export ADDRESS=$(cat ${PUBLICIP_FILE}):$(cat ${PORT_FORWARDING_STATUS_FILE})
     /app/entrypoint --server.address=0.0.0.0:$(cat ${PORT_FORWARDING_STATUS_FILE}) "$@"
 elif [[ -z "`jobs`" ]]; then
-    echo "gluetun died"
+    echo "gluetun died" 1>&2
     exit 1
 else
     sleep 10
